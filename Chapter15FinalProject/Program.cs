46274dd6f3c2183eb931f3b0e88830c04714d9b4
gidits13 <gidits13@gmail.com>
2022-10-26 14:27:05 +0300

    Добавьте файлы проекта.

---
+++ Chapter15FinalProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter15FinalProject
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+    internal class Program
+    {
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(x => x.Students.Select(s => s)).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+    }
+}
